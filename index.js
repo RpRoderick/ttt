c54082344b385a256d65902487f7ed005ede0cb5
RpRoderick <ryanproderick@gmail.com>
2016-11-02 13:42:32 -0400

    commit #1 of Javascript
    AHHHH! Javascript!! finally got the cell to display x.  need to
    assign players in order for function to work properly

--- index.js
+++ index.js
@@ -13,15 +13,3 @@ require('./assets/styles/index.scss');
 // attach jQuery globally
 require('expose?$!jquery');
 require('expose?jQuery!jquery');
-
-const getFormFields = require('../../lib/get-form-fields');
-const api = require('./api');
-const ui = require('./ui');
-
-$(() => {
-  $('#my-form').on('submit', function (e) {
-    let data = getFormFields(this);
-    e.preventDefault();
-    api.myRequest(data, ui.success, ui.failure);
-  });
-});

ead4a415f6cd7b221e499182a068f9588fae0b50
RpRoderick <ryanproderick@gmail.com>
2016-11-03 19:22:00 -0400

    commit #10
    all of my win combos are plotted, returns good console logs of win!

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -27,47 +27,80 @@ const world = require('../global.js');
 // };
 
 //CAN I JUST USE .SWITCH UNDER APPEND INSTEAD OF TOGGLING?
+const winCheck = function () {
+
+  let board = (world.ttt.board);
+
+  if (board[0] && (board[0] === board[1]) && (board[1] === board[2])) {
+    console.log('win');  //if these 3 values match=win.
+    $('.win-message').text(board[0] + ' wins!');
+    gameOver = true;   //if win, go to end game function
+    //if they don't match, continue playing
+  }
+  if (board[3] && (board[3] === board[4]) && (board[4] === board[5])){
+    console.log('win');
+    $('.win-message').text(board[0] + ' wins!');
+    gameOver = true;
+  }
+  if (board[6] && (board[6] === board[7]) && (board[7] === board[8])){
+    console.log('win');
+    $('.win-message').text(board[0] + ' wins!');
+    gameOver = true;
+  }
+  if (board[0] && (board[0] === board[3]) && (board[3] === board[6])){
+    console.log('win');
+    $('.win-message').text(board[0] + ' wins!');
+    gameOver = true;
+  }
+  if (board[1] && (board[1] === board[4]) && (board[4] === board[7])){
+    console.log('win');
+    $('.win-message').text(board[0] + ' wins!');
+    gameOver = true;
+  }
+  if (board[2] && (board[2] === board[5]) && (board[5] === board[8])){
+    console.log('win');
+    $('.win-message').text(board[0] + ' wins!');
+    gameOver = true;
+  }
+  if (board[0] && (board[0] === board[4]) && (board[4] === board[8])){
+    console.log('win');
+    $('.win-message').text(board[0] + ' wins!');
+    gameOver = true;
+  }
+  if (board[2] && (board[2] === board[4]) && (board[4] === board[6])){
+    console.log('win');
+    $('.win-message').text(board[0] + ' wins!');
+    gameOver = true;
+  }
+
+};
+
 
 const onTileClick = function () {
   let tile = $(this).attr('class');
-  // let tileID = '.td' + tile;
   let i = +(tile.replace(/\D/g, ''));
    if (world.ttt.player === 'x') {
      $('.'+tile).append('X');
      world.ttt.player = 'o';
+     world.ttt.board[i] = 'x';
   } else if (world.ttt.player === 'o') {
      $('.'+tile).append('O');
-     world.ttt.player = 'x';
+     world.ttt.player = 'x'; //swicthing player
+     world.ttt.board[i] = 'o';
   }
-    let board = (world.ttt.baord);
+    if (winCheck()) {
+ console.log('you win');
+    }
 
-    if (board[0] === board[1] === board[2]){  //if these 3 values match=win.
-      return true;   //if win, go to end game function
-      //if they don't match, continue playing
-    }
-    if (board[3] === board[4] === board[5]){
-      return true;
-    }
-    if (board[6] === board[7] === board[8]){
-      return true;
-    }
-    if (board[0] === board[3] === board[6]){
-      return true;
-    }
-    if (board[1] === board[4] === board[7]){
-      return true;
-    }
-    if (board[2] === board[5] === board[8]){
-      return true;
-    }
-    if (board[0] === board[4] === board[8]){
-      return true;
-    }
-    if (board[2] === board[4] === board[6]){
-      return true;
-    }
+
+    console.log(world.ttt.board);
 };
 
+
+// const endGame = function () {
+//
+// };
+//   //if win or board full, stop click function, declare winner
 
 
 const onSignUp = function (event) {
@@ -116,7 +149,6 @@ const addHandlers = () => {
   $('.ca6').one('click', onTileClick);
   $('.cb7').one('click', onTileClick);
   $('.cc8').one('click', onTileClick);
-  $('td').on('click', onUpdateGame);
 };
 
 module.exports = {
